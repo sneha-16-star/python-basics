1b3b9f98a995676ea83b9df948f2b4573fb8b5cc
sneha-16-star <farihatasnimsneha@gmail.com>
2025-10-25 21:36:42 +0600

    v1

---
+++ py 3/py activity 2.py
@@ -0,0 +1,12 @@
+# Creating different variables
+x = 5
+y = "john"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+print("nHello", name, "nwelcome to codingal")
