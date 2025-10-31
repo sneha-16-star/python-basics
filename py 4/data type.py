6c1c3317bdf46985fa3f8e42e2a45f5c1fd4ab39
sneha-16-star <farihatasnimsneha@gmail.com>
2025-10-31 21:36:53 +0600

    v2

--- py 4/data type.py
+++ py 4/data type.py
@@ -1,9 +1,10 @@
 # Let's check the datatype of different values
+a=4
 
-print(type of a: ", type(a))
+print("type of a: ", type(a))
 b=2.5
-print(type of b: ", type(b))
+print("type of b: ", type(b))
 c = "coding"
-print(type of c: ", type(c))
+print("type of c: ", type(c))
 d = True
-print(type of d: ", type(d))
+print("type of d: ", type(d))
