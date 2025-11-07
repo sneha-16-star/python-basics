41d36e94a5c7840e282b2b10b1de35c5341f3468
sneha-16-star <farihatasnimsneha@gmail.com>
2025-11-07 21:41:08 +0600

    v1

---
+++ py cond/c.py
@@ -0,0 +1,13 @@
+# Checking the number greater or smaller
+# Solution
+
+i = int(input("Enter a number : "))
+
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+else:
+    print("i is greater than 15")
+    print("i'm in else Block")
+
+print("i'm not in if and not in else Block")
